e1f54443a269c4f05b6a5fdadfa316db040140ae
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2024-08-20 09:48:59 +0300

    Lecture 2

---
+++ Introduction/Account.java
@@ -0,0 +1,54 @@
+package Introduction;
+
+public class Account {
+
+    // Instance variables
+    private String  name, accountNumber;
+    private double balance;
+    //class variable
+    static String bankName = "Alrajhi";
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setAccountNumber(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public void setBalance(Double balance){
+        if( balance <0){
+            System.out.println("balance must be >=0");
+        }else
+            this.balance = balance;
+    }
+    public double getBalance(){
+        return balance;
+    }
+
+    public void widthdraw(double amount){
+        if(balance >= amount){
+            balance -= amount;
+            System.out.println("Take the money ");
+        }else{
+            System.out.println("No fund ");
+        }
+
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+
+    }
+
+    public String toString(){
+        return String.format("Bank Name:%s%nName: %s%nAccount Number:%s%nBalance: %.2f%n", bankName, name, accountNumber, balance);
+    }
+
+}
---
+++ Introduction/Tester.java
@@ -0,0 +1,31 @@
+package Introduction;
+
+
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        Account acc1 ;
+        acc1 = new Account();
+        acc1.setAccountNumber("132342342342");
+        acc1.setName("Ahmad Ibraheam");
+        acc1.setBalance( 160.02);
+
+        System.out.printf(acc1.toString());
+
+        acc1.deposit(990);
+        System.out.printf(acc1.toString());
+        acc1.widthdraw(1000);
+        System.out.printf(acc1.toString());
+
+        acc1.setBalance(-500.00);
+        System.out.printf(acc1.toString());
+
+//        Account acc2 = new Account();
+//        acc2.accountNumber = "ACC2342342";
+//        acc2.name = "Muhammad Ibraheam";
+//        acc2.balance = 160000.02;
+//        System.out.printf(acc2.toString());
+    }
+}
